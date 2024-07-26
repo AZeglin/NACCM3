8d5fefec2e2be23a8b56887cfa2617d90a7e126c
Adrian Zeglin <147638089+AZeglin@users.noreply.github.com>
2024-07-26 17:29:00 -0500

    Add files via upload

---
+++ ApplicationStartup/JQuery/NACCMInit.js
@@ -0,0 +1,30 @@
+﻿/* NACCMInit.js - Loaded from ScriptManager scripts collection */
+/* must use Control.ClientID to reference controls inside an include file */
+
+
+
+
+function Nada(){
+
+    if (typeof (postbackElement) == "undefined") {
+        return;
+    }
+    alert('NACCMMasterPageLoaded A');
+    var rebateScrollValue = $get("rebateScrollPos").value;
+    alert('NACCMMasterPageLoaded B');
+    if (postbackElement.id.toLowerCase().indexOf('rebategridview') > -1) {
+        alert('NACCMMasterPageLoaded B 1');
+        $get("RebateGridViewDiv").scrollTop = rebateScrollValue;
+        alert('NACCMMasterPageLoaded B 2');
+
+    }
+    alert('NACCMMasterPageLoaded C');
+    highlightRebateRow();
+    alert('NACCMMasterPageLoaded end');
+
+}
+
+function saveClientScreenResolutionInfo() {
+    document.forms[0].ClientScreenHeight.value = screen.height;
+    document.forms[0].ClientScreenWidth.value = screen.width;      
+}
